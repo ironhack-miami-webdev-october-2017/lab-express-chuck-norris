7c2ceeeba7e8356ada5cf19bc5c148407ff4efe6
fontcuberta <isafontcu@gmail.com>
2017-01-11 17:49:32 +0100

    Second iteration

--- solution-code/app.js
+++ solution-code/app.js
@@ -15,9 +15,27 @@ console.log("Listening on port 3000");
 app.get('/random', (req, res) => {
   // Retrieve a random chuck joke
   client.getRandomJoke().then(function (response) {
-      console.log(response);
       res.render('index', response);
   }).catch(function (err) {
       // handle error
   });
+});
+
+// Second iteration
+app.get('/categories', (req, res) => {
+  client.getJokeCategories().then(function (response) {
+    let data = {categories: response};
+      res.render('categories', data);
+  }).catch(function (err) {
+      // handle error
+  });
+});
+
+app.get('/categories/:category', (req, res) => {
+  client.getRandomJoke(`${req.params.category}`).then(function (response) {
+    res.render('joke-by-category', response);
+  }).catch(function (err) {
+    // handle error
+  });
+
 });
