2a5c7e59c2ac560da11733eb32e3154df94f86ea
Raulvv <raulvprivate@gmail.com>
2017-01-12 16:00:39 +0100

    Finished third iteration without styling

--- solution-code/app.js
+++ solution-code/app.js
@@ -2,12 +2,18 @@ const express = require('express');
 const app = express();
 const Chuck  = require('chucknorris-io');
 const client = new Chuck();
+const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
 
+app.use(expressLayouts);
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('layout', 'layouts/main-layout');
 app.set('views', __dirname + "/views");
 app.set('view engine', 'ejs');
 
 app.listen(3000, () => {
-console.log("Listening on port 3000");
+  console.log("Listening on port 3000");
 });
 
 
@@ -16,8 +22,6 @@ app.get('/random', (req, res) => {
   // Retrieve a random chuck joke
   client.getRandomJoke().then(function (response) {
       res.render('index', response);
-  }).catch(function (err) {
-      // handle error
   });
 });
 
@@ -26,16 +30,28 @@ app.get('/categories', (req, res) => {
   client.getJokeCategories().then(function (response) {
     let data = {categories: response};
       res.render('categories', data);
-  }).catch(function (err) {
-      // handle error
   });
 });
 
 app.get('/categories/:category', (req, res) => {
   client.getRandomJoke(`${req.params.category}`).then(function (response) {
     res.render('joke-by-category', response);
-  }).catch(function (err) {
-    // handle error
   });
-
 });
+
+
+app.get('/search', (req, res) => {
+  let data = {joke: null};
+
+  res.render('search-form', data);
+});
+
+app.post('/search', (req, res) => {
+  let keyword = req.body.keyword;
+
+  CNClient.search(keyword).then( (response) => {
+    let randomIndex = Math.floor(Math.random() * response.items.length -1);
+    console.log(response.items[randomIndex]);
+    res.render('search-form', {joke: response.items[randomIndex]});
+  })
+})
