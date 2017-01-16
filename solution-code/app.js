45830bd98816e9bac8eb660aaf9e2cb0db4a54db
f3r <fer@ironhack.com>
2017-01-16 18:59:08 +0000

    improved solution

--- solution-code/app.js
+++ solution-code/app.js
@@ -1,8 +1,11 @@
 const express        = require('express');
 const app            = express();
+
 const expressLayouts = require('express-ejs-layouts');
 const bodyParser     = require('body-parser');
 const path           = require('path');
+
+// https://www.npmjs.com/package/chucknorris-io
 const Chuck          = require('chucknorris-io');
 const client         = new Chuck();
 
@@ -14,57 +17,46 @@ app.set('views', path.join(__dirname, "views"));
 app.set('view engine', 'ejs');
 app.set('layout', 'layouts/main-layout');
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000");
-});
-
 
 // First iteration
+app.get("/", (req, res) => {
+  res.redirect('/random');
+});
+
 app.get('/random', (req, res) => {
   // Retrieve a random chuck joke
-  client.getRandomJoke().then(function (response) {
-      res.render('index', response);
+  client.getRandomJoke().then(function (joke) {
+      res.render('index', {joke: joke.value});
   });
 });
 
 // Second iteration
 app.get('/categories', (req, res) => {
-  client.getJokeCategories().then(function (response) {
-    let data = {categories: response};
-      res.render('categories', data);
+  client.getJokeCategories().then(function (categories) {
+    res.render('categories', {categories});
   });
 });
 
 app.get('/categories/:category', (req, res) => {
-  client.getRandomJoke(`${req.params.category}`).then(function (response) {
-    res.render('joke-by-category', response);
+  let category = req.params.category;
+
+  client.getRandomJoke(category).then(function (joke) {
+    res.render('joke-by-category', {joke: joke.value, category});
   });
 });
 
 // Third iteration
-app.get('/search', (req, res) => {
-  let data = {joke: null};
-
-  res.render('search-form', data);
-});
-
 app.post('/search', (req, res) => {
   let keyword = req.body.keyword;
 
-  client.search(keyword).then( (response) => {
-    let randomIndex = Math.floor(Math.random() * response.items.length -1);
-    console.log(response.items[randomIndex]);
-    res.render('search-form', {joke: response.items[randomIndex]});
-  })
-})
+  client.search(keyword).then( (jokes) => {
+    let randomIndex = Math.floor(Math.random() * jokes.items.length -1);
+    let joke = jokes.items[randomIndex];
 
-app.get('/test', (requestObject, responseObject) => {
-  responseObject.send(`
-  <!doctype html>
-  <html>
-    <head>
-      <link rel="stylesheet" href="stylesheets/style.css">
-    </head>
-  </html>
-  `)
+    res.render('joke-by-search', {joke: joke.value, query: keyword} );
+  })
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
 });
